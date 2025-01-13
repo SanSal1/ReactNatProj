832aa7a130c028a4775102613779406ecfa87441
Salonen Sanna <sanna.salonen@etteplan.com>
2025-01-13 12:18:41 +0200

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Placeholder</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FFFFFF',
+  },
+})
